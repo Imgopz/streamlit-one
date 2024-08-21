a046faa39ace6a95352c5fa86d2eb250f5034c95
imgopz <rrgkmech@gmail.com>
2024-08-21 11:47:04 +0530

    st expander

--- first-app/app-charts.py
+++ first-app/app-charts.py
@@ -3,63 +3,87 @@ import streamlit as st
 import plotly.graph_objects as go 
 
          
+
+def makeTreemap(labels, parents):
+    data = go.Treemap(
+        ids=labels,
+        labels=labels,
+        parents=parents,
+        root_color="lightgray"
+    )
+    
+    fig = go.Figure(data)
+
+    return fig
+
+
+
+def makeIcicle(labels, parents):
+    data = go.Icicle(
+        ids=labels,
+        labels=labels,
+        parents=parents,
+        root_color="lightgray"
+    )
+
+    fig = go.Figure(data)
+    
+    return fig
+
+
+
+def makeSunburst(labels, parents):
+    data = go.Sunburst(
+        ids=labels,
+        labels=labels,
+        parents=parents,
+        
+        insidetextorientation='horizontal'
+    )
+
+    fig = go.Figure(data)
+
+    return fig
+
+
+
+def makeSankey(labels, parents):
+
+    data = go.Sankey(
+        node = dict(label=labels),
+        link=dict(
+            source=[list(labels).index(x) for x in labels],
+            target=[-1 if pd.isna(x) else list(labels).index(x) for x in parents],
+            label=labels,
+            value=list(range(1, len(labels)))))
+
+    fig = go.Figure(data)
+
+    return fig
+
 st.title("Hierarchical Data Charts")
 
 
 df = pd.read_csv("data/employees.csv", header=0).convert_dtypes()
-st.dataframe(df)
+# st.dataframe(df)
 
-labels = df[df.columns[0]]
-parents = df[df.columns[1]]
+labels, parents = df[df.columns[0]], df[df.columns[1]]
 
-data = go.Treemap(
-    ids=labels,
-    labels=labels,
-    parents=parents,
-    root_color="lightgray"
-)
+with st.expander("Treemap"):
+    fig = makeTreemap(labels, parents)
+    st.plotly_chart(fig, use_container_width=True)
 
-fig = go.Figure(data)
+with st.expander("Icicle"):
+    fig = makeIcicle(labels, parents)
+    st.plotly_chart(fig, use_container_width=True)
 
-st.plotly_chart(fig, use_container_width=True)
+with st.expander("Sunburst"):
+    fig = makeSunburst(labels, parents)
+    st.plotly_chart(fig, use_container_width=True)
 
-data = go.Icicle(
-    ids=labels,
-    labels=labels,
-    parents=parents,
-    root_color="lightgray"
-)
-
-fig = go.Figure(data)
-
-st.plotly_chart(fig, use_container_width=True)
-
-
-data = go.Sunburst(
-    ids=labels,
-    labels=labels,
-    parents=parents,
-    
-    insidetextorientation='horizontal'
-)
-
-fig = go.Figure(data)
-
-st.plotly_chart(fig, use_container_width=True)
-
-
-
-data = go.Sankey(
-    node = dict(label=labels),
-    link=dict(
-        source=[list(labels).index(x) for x in labels],
-        target=[-1 if pd.isna(x) else list(labels).index(x) for x in parents],
-        label=labels,
-        value=list(range(1, len(labels)))))
-
-fig = go.Figure(data)
-
-st.plotly_chart(fig, use_container_width=True)
+with st.expander("Sankey"):
+    fig = makeSankey(labels, parents)
+    st.plotly_chart(fig, use_container_width=True)
 
 
 
